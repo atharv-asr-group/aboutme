358d626230a113a30eb8da730b73d1227503574b
unknown <atharv21240@iiitd.ac.in>
2024-01-08 17:41:09 +0530

    dark mode shadcn

---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { ModeToggle } from './ui/toggleButton'
+export default function Nav(){
+    return (
+        <div>
+            <ModeToggle></ModeToggle>
+            
+        </div>
+    )
+}
